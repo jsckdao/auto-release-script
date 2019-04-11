4636a424a4d7a7070af6a320ecaf37b8368cbabe
jsckdao <jsckdao@gmail.com>
2019-04-11 11:30:48 +0800

    文件枷锁

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const { promisify } = require('util');
 const exec = promisify(child.exec);
 const exists = promisify(fs.exists);
 const mkdir = promisify(fs.mkdir);
+const writeFile = promisify(fs.writeFile);
+const unlink = promisify(fs.unlink);
 
 const preProjectDir = Path.join(__dirname, 'pre-projects');
 const prodProjectDir = Path.join(__dirname, 'prod-projects');
@@ -17,11 +19,20 @@ const prodProjectDir = Path.join(__dirname, 'prod-projects');
 app.use(express.json());
 
 app.post('/payload', async (req, res) => {
+    const { action, repository, release } = req.body;
+    if (action !== 'published') {
+        return res.json({ message: 'do not thing' });
+    }
+
+    // 创建一个锁文件, 不允许同时操作同一个项目
+    const lockFile = Path.join(os.tmpdir(), 'auto-update-project-' + repository.name + '.lock');
+    if (await exists(lockFile)) {
+        return res.json({ message: 'locking' });
+    } else {
+        await writeFile(lockFile, 'locking', 'utf8');
+    }
+
     try {
-        const { action, repository, release } = req.body;
-        if (action !== 'published') {
-            return res.json({ message: 'do not thing' });
-        }
         const basePath = release.prerelease ? preProjectDir : prodProjectDir;
         const projectPath = Path.join(basePath, repository.name);
         if (!(await exists(basePath))) {
@@ -39,9 +50,15 @@ app.post('/payload', async (req, res) => {
         console.error(err);
         res.sendStatus(403);
         res.json({ message: err.message });
+    } finally {
+        // 移除锁文件
+        if (await exists(lockFile)) {
+            await unlink(lockFile);
+        }
     }
 });
 
+app.listen(8743);
 
 
 
